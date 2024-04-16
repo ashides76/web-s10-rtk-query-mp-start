d48f0f405c1a7c7d4d14531833df2497abced2b6
Ashish Desai <desai.ashish76@gmail.com>
2024-04-15 21:37:44 -0400

    created 4 endpoints, connted reducer and middleware, completed getquotes to render data

--- frontend/state/quotesApi.js
+++ frontend/state/quotesApi.js
@@ -1,1 +1,27 @@
 // create your RTK Query endpoints here
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+export const quotesApi = createApi({
+    reducerPath: 'quotesApi',
+    baseQuery: fetchBaseQuery({baseUrl: 'http://localhost:9009/api/'}),
+    tagTypes: ['Quotes'],
+    endpoints: build => ({
+        getQuotes: build.query({
+            query: () => 'quotes',
+            providesTags: ['Quotes']
+        }),
+        createQuote: build.mutation({
+
+        }),
+        toggleFake: build.mutation({
+
+        }),
+        deleteQuote: build.mutation({
+
+        }),
+    })
+})
+
+export const {
+    useGetQuotesQuery, 
+} = quotesApi
